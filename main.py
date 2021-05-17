2dfbdbbe6f21919629b6bf954e69e531df5761d3
Carlos <cespinal101@gmail.com>
2021-05-17 17:17:04 -0500

    version 1, webhook added

--- main.py
+++ main.py
@@ -6,6 +6,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+Instance = vlc.Instance('--fullscreen','--input-repeat=1','--mouse-hide-timeout=0')
+player = Instance.media_player_new()
+
+
+
+def play(path):
+    global player
+    Media = Instance.media_new(path)
+    player.set_media(Media)
+    Media.get_mrl()
+    player.set_fullscreen(True)
+    player.play()
+    sleep(5)
+    while player.is_playing():
+        sleep(1)
+
+def stop():
+    global player
+    player.stop()
+
+play("./clips/vibes.mkv")
 @app.route('/')
 def test():
     return "Howdy world"
+@app.route('/vibeone')
+def vibe_1():
+    play("./clips/vibes.mkv")
+    return 'done'
+@app.route('/stop')
+def end():
+    stop()
+    return 'done'
