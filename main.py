03cfd11432540952fb37f1a0418f2a394c671bd0
Carlos <cespinal101@gmail.com>
2021-05-18 00:07:41 -0500

    undid previous push

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ player = Instance.media_player_new()
 
 def play(path):
     global player
-    if(player.is_playing()):
-        player.stop()
     Media = Instance.media_new(path)
     player.set_media(Media)
     Media.get_mrl()
